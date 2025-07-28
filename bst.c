d1b10d073051ee64d2e1e3a48b5d14c1505fdaef
tajmul-4u <tajmulislam137@gmail.com>
2025-07-28 14:40:52 +0600

    BST

--- bst.c
+++ bst.c
@@ -23,3 +23,42 @@ struct Node*insert(struct Node*root,int value){
     root->right = insert(root->right,value);
     return root;
 }
+
+int search(struct Node*root,int key){
+    if(root == NULL)
+    return 0;
+    if (root->data == key)
+    return 1;
+    if(key < root->data)
+    return search(root->left,key);
+    else
+    return search(root->right,key);
+}
+
+void inorder(struct Node*root){
+    if(root != NULL){
+        inorder(root->left);
+        printf("%d",root->data);
+        inorder(root->right);
+    }
+}
+
+int main(){
+    struct Node*root = NULL;
+    int values[] = {50,30,20,40,70,60,70};
+    int n = sizeof(values)/sizeof(values[0]);
+    for(int i = 0; i<n; i++)
+    {
+        root = insert(root, values[i]);
+    }
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\n");
+    int key = 60;
+    if (search(root,key))
+    printf("Key %d found in BST.\n",key);
+    else
+    printf("Key %d not found in BST.\n",key);
+    
+    return 0;
+}
